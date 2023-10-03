8b5d26e66f4ccca34760132a22e02e689d23c3ca
JacobChurch2 <jchurch@student.neumont.edu>
2023-10-03 12:32:47 -0600

    GL's Hello World 2

--- Source/Application/World01.cpp
+++ Source/Application/World01.cpp
@@ -21,10 +21,12 @@ namespace nc
     void World01::Update(float dt)
     {
         m_angle += 90 * dt;
-        m_position.x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_A) ? -dt : 0;
-        m_position.x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_D) ? +dt : 0;
-        m_position.y += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_W) ? +dt : 0;
-        m_position.y += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_S) ? -dt : 0;
+        for (int i = 0; i < 10; i++) {
+            m_positions[i].x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_A) ? -dt : 0;
+            m_positions[i].x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_D) ? +dt : 0;
+            m_positions[i].y += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_W) ? +dt : 0;
+            m_positions[i].y += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_S) ? -dt : 0;
+        }
         m_time += dt;
     }
 
