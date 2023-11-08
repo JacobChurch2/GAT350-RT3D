5a47e665685a401823a15ebc850737327aaa898e
JacobChurch2 <jchurch@student.neumont.edu>
2023-11-08 13:33:40 -0700

    Rotate/json update

--- Source/Application/World06.cpp
+++ Source/Application/World06.cpp
@@ -24,55 +24,10 @@ namespace nc
         ADD_RESOURCE("fb", framebuffer);
 
         auto material = GET_RESOURCE(Material, "materials/framebuffer.mtrl");
-        if (material) 
+        if (material)
         {
             material->albedoTexture = texture;
         }
-        // CAMERA
-        {
-            auto actor = CREATE_CLASS(Actor);
-            actor->name = "camera1";
-            actor->transform.position = glm::vec3{ 0, 0, 3 };
-            actor->transform.rotation = glm::radians(glm::vec3{ 0, 180, 0 });
-
-            auto cameraComponent = CREATE_CLASS(CameraComponent);
-            cameraComponent->SetPerspective(70.0f, ENGINE.GetSystem<Renderer>()->GetWidth() / (float)ENGINE.GetSystem<Renderer>()->GetHeight(), 0.1f, 100.0f);
-            actor->AddComponent(std::move(cameraComponent));
-
-            auto cameraController = CREATE_CLASS(CameraController);
-            cameraController->speed = 5;
-            cameraController->sensitivity = 0.5f;
-            cameraController->m_owner = actor.get();
-            cameraController->Initialize();
-            actor->AddComponent(std::move(cameraController));
-
-            m_scene->Add(std::move(actor));
-        }
-
-        //LIGHT
-        {
-            auto actor = CREATE_CLASS(Actor);
-            actor->name = "light1";
-            actor->transform.position = glm::vec3{ 3, 3, 3 };
-            auto lightComponent = CREATE_CLASS(LightComponent);
-            lightComponent->type = LightComponent::eType::Point;
-            lightComponent->color = glm::rgbColor(glm::vec3{ randomf() * 360, 1, 1 });
-            lightComponent->intensity = 1;
-            lightComponent->range = 20;
-            lightComponent->innerAngle = 10.0f;
-            lightComponent->outerAngle = 30.0f;
-            actor->AddComponent(std::move(lightComponent));
-            m_scene->Add(std::move(actor));
-        }
-     /*   for (int i = 0; i < 10; i++) {
-            auto actor = CREATE_CLASS_BASE(Actor, "tree");
-            actor->name = StringUtils::CreateUnique("tree");
-            actor->transform.position = glm::vec3{ randomf(-10,10), 0, randomf(-10,10) };
-            actor->transform.scale = glm::vec3{ randomf(0.5f, 3.0f), randomf(0.5f, 5.0f), 1 };
-            actor->Initialize();
-            m_scene->Add(std::move(actor));
-        }
-        */
 
         return true;
     }
